644cb9c3e5875513396a43be77bbe9dc7ce5bb35
Brandon Smith <mail@brandonsmith.ninja>
2019-11-02 14:43:45 -0500

    Found out compiler optimizations weren't enabled, got a 12x speedup, rendered a juicier image

--- src/fidelity_consts.rs
+++ src/fidelity_consts.rs
@@ -1,12 +1,12 @@
 
 // Image resolution, in pixels.
-pub const RESOLUTION: usize = 256;
+pub const RESOLUTION: usize = 1024;
 
 // Number of sample rays to cast for diffuse/specular illumination
-pub const SAMPLE_COUNT: usize = 8;
+pub const SAMPLE_COUNT: usize = 32;
 
 // Max number of indirect bounces to make
-pub const MAX_DEPTH: u8 = 4;
+pub const MAX_DEPTH: u8 = 3;
 
 // How many chunks the image should be split up into, for multithreading
 pub const CELLS: usize = 64; // must be the square of an integer
