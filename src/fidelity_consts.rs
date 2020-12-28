334ef4367a3010e3485726918035f821eecdebe3
Brandon Smith <mail@brandonsmith.ninja>
2020-12-28 17:30:59 -0600

    Make constants a bit more semantic

--- src/fidelity_consts.rs
+++ src/fidelity_consts.rs
@@ -1,11 +1,12 @@
 
 // Image resolution, in pixels.
 // 3072 x 1920; 16:10
-pub const RESOLUTION_Y: usize = 1920; // 1920
-pub const RESOLUTION_X: usize = 1 + (RESOLUTION_Y as f32 * 1.6) as usize;
+pub const ASPECT_RATIO: f32 = 16.0 / 10.0;
+pub const RESOLUTION_Y: usize = 64; // 1920
+pub const RESOLUTION_X: usize = 1 + (RESOLUTION_Y as f32 * ASPECT_RATIO) as usize;
 
 // Number of sample rays to cast for diffuse/specular illumination
-pub const SAMPLE_COUNT: usize = 16;
+pub const SAMPLE_COUNT: usize = 32;
 
 // Max number of indirect bounces to make
 pub const BOUNCES: u8 = 2;
